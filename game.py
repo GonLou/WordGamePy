b8313cd81fb7d6eb5aebe3f4578cbe097d8716bb
GonLou <gnunodal@hotmail.com>
2015-10-07 22:35:09 +0100

    directions N S E W

--- game.py
+++ game.py
@@ -4,6 +4,18 @@ class Game(cmd.Cmd):
 	def __init__(self):
 		cmd.Cmd.__init__(self)
 		
+	def do_n(self, args):
+		"""Go north"""
+		
+	def do_s(self, args):
+		"""Go south"""
+		
+	def do_e(self, args):
+		"""Go east"""
+
+	def do_w(self, args):
+		"""Go west"""
+		
 	def do_quit(self, args):
 		print("End of game")
 		return True
