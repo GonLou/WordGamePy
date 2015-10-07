fa28b0ede80a2b2e96b5cb1d58629524a7ee68d9
GonLou <gnunodal@hotmail.com>
2015-10-07 22:28:35 +0100

    cmd implemented

---
+++ game.py
@@ -0,0 +1,13 @@
+import cmd
+
+class Game(cmd.Cmd):
+	def __init__(self):
+		cmd.Cmd.__init__(self)
+		
+	def do_quit(self, args):
+		print("End of game")
+		return True
+		
+if __name__ == "__main__":
+	g = Game()
+	g.cmdloop()
